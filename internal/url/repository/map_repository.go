043e4c6acfe2ffc52a4a0a22f04632f504370c6b
MatiXxD <ilya_dinesenko@mail.ru>
2025-02-13 04:17:42 +0300

    add client

--- internal/url/repository/map_repository.go
+++ internal/url/repository/map_repository.go
@@ -1,30 +1,39 @@
 package repository
 
 import (
+	"sync"
+
 	"github.com/MatiXxD/url-shortener/internal/models"
 	"github.com/MatiXxD/url-shortener/internal/url"
 )
 
 type MapRepository struct {
 	db map[string]*models.URL
+	mu sync.RWMutex
 }
 
 func NewMapRepository(d map[string]*models.URL) url.Repository {
 	return &MapRepository{
 		db: d,
+		mu: sync.RWMutex{},
 	}
 }
 
 func (mr *MapRepository) AddURL(url, shortURL string) (string, error) {
+	mr.mu.Lock()
+	defer mr.mu.Unlock()
+
 	if got, ok := mr.db[url]; ok {
 		return got.ShortURL, nil
 	}
-
 	mr.db[url] = models.NewURL(url, shortURL)
 	return shortURL, nil
 }
 
 func (mr *MapRepository) GetURL(shortURL string) (string, bool) {
+	mr.mu.RLock()
+	defer mr.mu.RUnlock()
+
 	for k, v := range mr.db {
 		if v.ShortURL == shortURL {
 			return k, true
